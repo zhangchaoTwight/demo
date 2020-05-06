08693fed0051d3b137283e937fbcff641014f382
zhangchao <895498206@qq.com>
2020-05-06 14:02:03 +0800

    hello world

---
+++ HelloWord.java
@@ -0,0 +1,12 @@
+Public classHelloWorld{
+	public static void main(String[] args ){
+		System.out.println("Hello world1");
+		System.out.println("Hello world6");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
+		System.out.println("Hello world5");
+		System.out.println("Hello world4");
+		
+		
+	} 
+}
